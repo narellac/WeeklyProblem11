00dc8abed6546276544ab80073f5b535b9ea9024
Narella Calvente <narellacalvente@gmail.com>
2021-04-17 17:01:32 -0300

    BQA-11: library

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors());
+app.use(express.json());
+const port = 4000
+
+let user = null;
+
+app.post('/register', (req, res) => {
+    user = req.body;
+    res.json({result: 'success'})
+})
+
+app.put('/login', (req, res) => {
+    const body = req.body;
+    if(user && user.email === body.email && user.password === body.password) {
+        res.json({result: 'success'})
+    } else {
+        res.json({result: 'error'})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
