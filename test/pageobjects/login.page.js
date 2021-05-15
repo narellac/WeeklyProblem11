6f05415fc2838070a307356b332896ee6c8f80da
Narella Calvente <narellacalvente@gmail.com>
2021-05-15 12:32:42 -0300

    BQA-14: corrections

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -4,21 +4,36 @@ const page = require ('../pageobjects/page');
 class LoginPage extends page {
 
     //login credentials selectors
-    get email () { return $('#email') }
-    get password () { return $('#password') }
+    get inputEmail () { return $('#email') }
+    get inputPassword () { return $('#password') }
 
     //buttons selectors
     get btnSubmit () { return $('#submit') }
     get btnReset () { return $('#reset') }
 
     //alerts selectors
-    get alertEmail () {return $('#error1')}
-    get alertPassword () {return $('#error2')}
-    get alertResult () {return $ ('#hidden')}
+    get alertEmail () { return $('#error1') }
+    get alertPassword () { return $('#error2') }
+    get alertResult () { return $ ('#hidden') }
+
+    login (email, password) {
+      this.inputEmail.setValue(email);
+      this.inputPassword.setValue(password);
+    }
+    setEmail (value){
+        this.inputEmail.click();
+        this.inputEmail.setValue(value);
+        this.inputEmail.keys("Tab");
+    }
+    setPassword (value){
+        this.inputPassword.click();
+        this.inputPassword.setValue(value);
+        this.inputPassword.keys("Tab");
+    }
 
     //login path
     open () {
-        return super.open('https://narellac.github.io/WeeklyProblem09/login.html');
+        return browser.url('https://narellac.github.io/WeeklyProblem09/login.html');
     }
     submit () {
         this.btnSubmit.click()
--- test/specs/login.js
+++ test/specs/login.js
@@ -1,56 +1,62 @@
 const LoginPage = require ('../pageobjects/login.page');
 
-describe('Login buttons', () => {
-    it('should verify that the all the buttons in the form are enabled', () => {
-        LoginPage.open ();
-        expect(LoginPage.btnSubmit).toBeClickable;
-        expect(LoginPage.btnReset).toExist;
-        expect(LoginPage.btnReset).toBeClickable;
+describe('Login TESTS', () => {
+  beforeAll('Open browser', () => {
+    LoginPage.open();
+  });
+  describe('Login buttons', () => {
+      it('should verify that the all the buttons in the form are enabled', () => {
+          expect(LoginPage.btnSubmit).toBeClickable;
+          expect(LoginPage.btnReset).toExist;
+          expect(LoginPage.btnReset).toBeClickable;
+          browser.pause(2000);
+      });
+  });
+  describe("Login email testing", () => {
+    it("if email input is empty show the proper message", () => {
+        LoginPage.inputEmail.click();
+        LoginPage.inputEmail.keys("Tab");
+        expect(LoginPage.alertEmail).toHaveText("The email must contain a valid format");
+        browser.pause(3000);
+    });
+    it("if email is invalid show the proper message", () => {
+      LoginPage.setEmail("invalid");
+      expect(LoginPage.alertEmail).toHaveText("Invalid Email");
+      browser.pause(2000);
+    });
+  it("if email is valid show the proper mssg", () => {
+      LoginPage.setEmail("narella@gmail.com");
+      expect(LoginPage.alertEmail).toHaveText("Correct");
+      browser.pause(2000);
+    });
+  });
+  describe("Login password testing", () => {
+    it("if password input is empty show the proper mssg", () => {
+        LoginPage.inputPassword.click();
+        LoginPage.inputPassword.keys("Tab");
+        expect(LoginPage.alertPassword).toHaveText("Doesn't match the password");
+        browser.pause(3000);
+    });
+    it("if password is invalid show the proper mssg", () => {
+        LoginPage.setPassword("invalidpass");
+        expect(LoginPage.alertPassword).toHaveText("Doesn't match the password");
         browser.pause(2000);
-    })
-});
-describe('Login credentials verification', () => {
-    it('should deny access when providing both a wrong username and a wrong passcode', () => {
-        LoginPage.open ();
-        LoginPage.email.setValue('narella@')
-        LoginPage.password.setValue('Contraseña')
-        LoginPage.submit()
-        expect(LoginPage.alertEmail).toHaveText('The email must contain a valid format')
-        expect(LoginPage.alertPassword).toHaveText('The password must contain at least 8 characters, consisting of letters and numbers')
+    });
+    it("if password is valid show the proper mssg", () => {
+        LoginPage.setPassword("password01");
+        expect(LoginPage.alertPassword).toHaveText("Correct");
         browser.pause(2000);
-    })
+    });
 });
-describe('Login credentials verification two', () => {
-    it('should deny access when providing a correct username and a wrong password', () => {
-        LoginPage.open ();
-        LoginPage.email.setValue('narella@gmail.com')
-        LoginPage.password.setValue('Contraseña')
-        LoginPage.submit()
-        expect(LoginPage.alertPassword).toHaveText('The password must contain at least 8 characters, consisting of letters and numbers')
-        browser.pause(2000);
-    })
+  describe('Login form', () => {
+      it('should allow access when providing correct inputs', () => {
+          LoginPage.inputEmail.setValue('narella@gmail.com')
+          LoginPage.inputPassword.setValue('Contraseña1122')
+          LoginPage.submit()
+          expect(LoginPage.alertPassword).not.toBeDisplayed('The password must contain at least 8 characters, consisting of letters and numbers')
+          expect(LoginPage.alertEmail).not.toBeDisplayed('The email must contain a valid format')
+          expect(LoginPage.alertResult).toBeDisplayed('Your login data is: narella@gmail.com  Contraseña1122')
+          browser.pause(2000);
+      });
+  });
 });
-describe('Login credentials verification three', () => {
-    it('should deny access when providing a wrong username and leaving the passcode field empty', () => {
-        LoginPage.open ();
-        LoginPage.email.setValue('Narella')
-        LoginPage.password.setValue('')
-        LoginPage.submit()
-        expect(LoginPage.alertEmail).toHaveText('The email must contain a valid format')
-        expect(LoginPage.alertPassword).toHaveText('The password must contain at least 8 characters, consisting of letters and numbers')
-        browser.pause(2000);
-    })
-});
-describe('Login form', () => {
-    it('should allow access when providing correct inputs', () => {
-        LoginPage.open ();
-        LoginPage.email.setValue('narella@gmail.com')
-        LoginPage.password.setValue('Contraseña1122')
-        LoginPage.submit()
-        expect(LoginPage.alertPassword).not.toBeDisplayed('The password must contain at least 8 characters, consisting of letters and numbers')
-        expect(LoginPage.alertEmail).not.toBeDisplayed('The email must contain a valid format')
-        expect(LoginPage.alertResult).toBeDisplayed('Your login data is: narella@gmail.com  Contraseña1122')
-        browser.pause(2000);
-    })
-});
-
